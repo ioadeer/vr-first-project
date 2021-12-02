b741e199ca93a42177ccfce4b34e738736329dd8
ioadeer <cjoackin@gmail.com>
2021-12-02 17:55:02 -0300

    first set of files

---
+++ Source/First_project/First_projectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "First_projectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRST_PROJECT_API AFirst_projectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
